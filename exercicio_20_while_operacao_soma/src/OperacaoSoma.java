ea7c29e45c0bc593a401549d86df24193252f9e5
JesskaBasilio <jessica.limabasilio@gmail.com>
2024-01-19 19:05:30 -0300

    initial commit

---
+++ exercicio_20_while_operacao_soma/src/OperacaoSoma.java
@@ -0,0 +1,18 @@
+
+public class OperacaoSoma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 0;
+		int y = 4;
+		
+		while (x < 3) {
+			y = y + 2;
+			x = x + 1;
+			System.out.println(x + "-" + y);
+		}
+
+	}
+
+}
