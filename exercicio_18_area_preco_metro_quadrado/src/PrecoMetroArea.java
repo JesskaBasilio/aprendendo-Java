05acea7ba8b01a87cdeb12761f43e2c9c700ddf1
JesskaBasilio <jessica.limabasilio@gmail.com>
2024-01-19 19:03:17 -0300

    doc: update

--- exercicio_18_area_preco_metro_quadrado/src/PrecoMetroArea.java
+++ exercicio_18_area_preco_metro_quadrado/src/PrecoMetroArea.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 
 public class PrecoMetroArea {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+		a = sc.nextInt();
+		
+		int area = a * a;
+		
+		System.out.print("Area: " + area + " metros quadrados");
+		
+		sc.close();
+		
 
 	}
 
