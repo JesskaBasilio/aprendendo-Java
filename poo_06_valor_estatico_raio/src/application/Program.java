536a920f558f00429eea7ad8bf9a7a43261246cf
JesskaBasilio <jessica.limabasilio@gmail.com>
2024-01-25 08:06:35 -0300

    doc: update class

--- poo_06_valor_estatico_raio/src/application/Program.java
+++ poo_06_valor_estatico_raio/src/application/Program.java
@@ -1,9 +1,31 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculator;
+
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.println("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = Calculator.circunference(radius);
+		
+		double v = Calculator.volume(radius);
+		
+		System.out.printf("Circunference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
+				
+		sc.close();
+
 
 	}
 
