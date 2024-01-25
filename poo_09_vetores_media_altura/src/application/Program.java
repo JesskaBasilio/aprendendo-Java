9aff55db215c61a24b2b7a71ccfa612fb2ecce8d
JesskaBasilio <jessica.limabasilio@gmail.com>
2024-01-25 08:29:19 -0300

    initial commit

---
+++ poo_09_vetores_media_altura/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		double[] vect = new double[n];
+		for (int i = 0; i < n; i ++) {
+			vect[i] = sc.nextDouble();
+		}
+		
+		double sum = 0.0;
+		for (int i = 0; i < n; i++) {
+			sum += vect[i];
+		}
+		
+		double media = sum/n;
+		System.out.printf("Media das alturas: %.2f", media);
+		
+		sc.close();
+
+	}
+
+}
