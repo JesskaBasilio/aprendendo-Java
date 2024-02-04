ab9efe18497d465d317096d02436c9f358fc5403
JesskaBasilio <jessica.limabasilio@gmail.com>
2024-02-04 17:32:19 -0300

    initial commit

---
+++ poo_12_pensionato/src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rent;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Rent[] vect = new Rent[10];
+		
+		System.out.print("Quantos quartos serão alugados? ");		
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println();
+			System.out.println("Rent #" + i + ":");
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.next();
+			System.out.print("Room: ");
+			int roomNumber = sc.nextInt();
+			
+			vect[roomNumber] = new Rent(name, email);
+		}
+		
+		System.out.println();
+		System.out.println("Quartos ocupados: ");
+		for (int i = 0; i < 10; i++) {
+			if(vect[i] != null) {
+				System.out.println(i + ": " + vect[i]);
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
