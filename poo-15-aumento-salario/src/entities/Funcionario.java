c33c3d40e5613175eba9c3308b50f97ce99ababa
JesskaBasilio <jessica.limabasilio@gmail.com>
2024-02-27 20:57:49 -0300

    doc: updating funcionario class

--- poo-15-aumento-salario/src/entities/Funcionario.java
+++ poo-15-aumento-salario/src/entities/Funcionario.java
@@ -3,4 +3,51 @@ package entities;
 public class Funcionario {
 	
 	private int id;
+	public String name;
+	public double salary;
+	
+	public Funcionario() {
+		
+	}
+
+
+	public Funcionario(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+
+	
 }
