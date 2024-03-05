5766c2133396514bd9036f7303160b18db84a2e1
JesskaBasilio <jessica.limabasilio@gmail.com>
2024-03-05 11:34:06 -0300

    initial commit

---
+++ estagio_ribeirao_preto_gupy/src/application/SomaIndices.java
@@ -0,0 +1,19 @@
+package application;
+
+public class SomaIndices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int INDICE = 13, SOMA = 0, K = 0;
+
+        while (K < INDICE) {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        System.out.println(SOMA);
+    	}
+
+	}
+
+//Resposta: 91 (esse é o resultado da soma)
