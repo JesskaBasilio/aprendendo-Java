c6bcbc9527a59f0e2db62d105af3e600381d387d
JesskaBasilio <jessica.limabasilio@gmail.com>
2024-03-06 12:20:53 -0300

    initial commit

---
+++ poo_15_matrizes/src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[][] mat = new int[n][n];
+		
+		for(int i=0; i < n; i++) {
+			for(int j=0; j < n; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Main diagonal: ");
+		for(int i=0; i < n; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		
+		System.out.println();
+		
+		int count = 0;
+		for(int i=0; i < n; i++) {
+			for(int j=0; j < n; j++) {
+				if (mat[i][j] < 0) {
+					count++;
+				};
+			}
+		}
+		
+		System.out.print("Negative numbers = " + count);
+		
+		
+		sc.close();
+
+	}
+
+}
