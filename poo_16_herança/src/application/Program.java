6b4653e632ff4741133f7dfe4e73a7024b05df59
JesskaBasilio <jessica.limabasilio@gmail.com>
2024-03-15 10:00:45 -0300

    doc: downcasting

--- poo_16_herança/src/application/Program.java
+++ poo_16_herança/src/application/Program.java
@@ -17,7 +17,24 @@ public class Program {
 		//atribuindo um objeto da subclasse para a superclasse
 		Account acc2 = new BusinessAccount(1003, "Fernanda", 0.0, 200.0);
 		Account acc3 = new SavingsAccount(1004, "Patricia", 0.0, 0.01);
-
+		
+		//DOWNCASTING
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.Loan(100.0);
+		
+		//BusinessAccount acc5 = (BusinessAccount)acc3;
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.Loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 	}
 
 }
